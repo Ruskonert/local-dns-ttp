88fa9fcdeea92e0d8113fbea01816bbed6200435
Ruskonert <ruskonert@gmail.com>
2019-11-14 13:18:20 +0900

    Updated: Source code for quic-implementation

--- client.py
+++ client.py
@@ -1,8 +1,16 @@
 import sys
 import socket
+import quic
 
 def main(argc, argv):
-    pass
+    if argc == 1:
+        print("Usuge:")
+        print("client.py <TTP_DNS_ADDRESS> <PORT>")
+    else:
+        dns_addr = sys.argv[1]
+        port = int(sys.argv[2])
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 
 if __name__ == "__main__":
     main(len(sys.argv), sys.argv)
